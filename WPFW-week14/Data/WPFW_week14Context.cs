a108b1a25936a05ef9aca28a51f7c89885baf916
Mehdi <Mehdi@DESKTOP-QL788C6>
2020-12-07 21:07:38 +0100

    added many-many relation, scaffold all new models and migrations fixed

---
+++ WPFW-week14/Data/WPFW_week14Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WPFW_week14.Models;
+
+namespace WPFW_week14.Data
+{
+    public class WPFW_week14Context : DbContext
+    {
+        public WPFW_week14Context (DbContextOptions<WPFW_week14Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WPFW_week14.Models.Student> Student { get; set; }
+
+        public DbSet<WPFW_week14.Models.Course> Course { get; set; }
+
+        public DbSet<WPFW_week14.Models.StudentCourse> StudentCourse { get; set; }
+    }
+}
---
+++ WPFW-week14/Models/StudentCourse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WPFW_week14.Models
+{
+    public class StudentCourse
+    {
+        public int Id { get; set; }
+        public Student Student { get; set; }
+        public Course Course { get; set; }
+    }
+}
