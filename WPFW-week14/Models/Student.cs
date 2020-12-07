a6befecf3376debb3ab17075c2b218a5d89d6750
Mehdi <Mehdi@DESKTOP-QL788C6>
2020-12-07 20:43:18 +0100

    added models student and course

---
+++ WPFW-week14/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WPFW_week14.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double length { get; set; }
+    }
+}
